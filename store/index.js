48094bef283682dd6b98431d0a8a80942bcaf692
jisungyou <iujisu@naver.com>
2023-04-23 14:34:09 +0900

    store 추가

--- store/index.js
+++ store/index.js
@@ -1,12 +1,34 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import router from '@/router';
 
 export const store = createStore ({
   state: {
+    // 상태 관리하는 곳
+    userId: '',
+    userName: '',
+    token: ''
   },
-  getters: {
+  getters: { 
+     // 컴포넌트에서 state를 사용할 수 있게 해주는 곳
   },
   mutations: {
+    // state 변경하는 곳
+    login: function (state, payload) {
+        state.userId = payload.userId
+        state.userName = payload.userName
+        state.token = payload.token
+    },
+    loginCheck: function (state) {
+        if (!state.token) {
+            router.push({
+                name: 'login'
+            }).catch(error => {
+                console.log(error)
+            })
+        }
+    }
   },
-  actions: {
-  },
+  actions: { 
+     //비동기 요청, mutations 호출
+   }
 })
